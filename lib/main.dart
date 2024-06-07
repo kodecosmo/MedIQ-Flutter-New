089c4b8c0330f859482ec6ee5e98bc603ae85fb8
Shakthi Warnakulasuriya <shakthi.20221274@iit.ac.lk>
2024-06-07 12:09:02 +0530

    feat: Update main.dart with SOS screen and dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:mediq_flutter/screen/instructions/step3.dart';
 import 'package:mediq_flutter/screen/instructions/step4.dart';
 import 'package:mediq_flutter/screen/instructions/step5.dart';
 import 'package:mediq_flutter/screen/login.dart';
+import 'package:mediq_flutter/screen/sos.dart';
 import 'package:mediq_flutter/screen/welcome_screen.dart';
 import 'package:mediq_flutter/screen/splash.dart';
 
@@ -21,35 +22,34 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'MediQ',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: ChatbotPage(),
-      routes: {
-       // '/': (context) => const SplashScreen(),
-        '/welcome': (context) => const WelcomeScreen(),
-        '/login': (context) =>  LoginPage(),
-        '/onboarding': (context) => const OnBoardingScreen(),
-        '/chatbot': (context) => const ChatbotPage(),
-      }
-    );
+        title: 'MediQ',
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // TRY THIS: Try running your application with "flutter run". You'll see
+          // the application has a purple toolbar. Then, without quitting the app,
+          // try changing the seedColor in the colorScheme below to Colors.green
+          // and then invoke "hot reload" (save your changes or press the "hot
+          // reload" button in a Flutter-supported IDE, or press "r" if you used
+          // the command line to start the app).
+          //
+          // Notice that the counter didn't reset back to zero; the application
+          // state is not lost during the reload. To reset the state, use hot
+          // restart instead.
+          //
+          // This works for code too, not just values: Most code changes can be
+          // tested with just a hot reload.
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const Sos(),
+        routes: {
+          // '/': (context) => const SplashScreen(),
+          '/welcome': (context) => const WelcomeScreen(),
+          '/login': (context) => LoginPage(),
+          '/onboarding': (context) => const OnBoardingScreen(),
+          '/chatbot': (context) => const ChatbotPage(),
+        });
   }
 }
 
