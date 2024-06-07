e40ed9e509efda573896434c26647e63075a4633
Buddhima Fernando <harun.fernando.hf@gmail.com>
2024-06-07 23:39:46 +0530

    refactor: Comment out unused code in utils.dart

--- lib/screen/user_calls/utils.dart
+++ lib/screen/user_calls/utils.dart
@@ -1,4 +1,4 @@
-class Utils {
-  static int appId = 1606773506;
-  static String appSignIn = "002c75ac0aa5beeb0d20aacdf8dcdd0ce239404bee73d10b8b3d6daf979875a2";
-}
+// class Utils {
+//   static int appId = 1606773506;
+//   static String appSignIn = "002c75ac0aa5beeb0d20aacdf8dcdd0ce239404bee73d10b8b3d6daf979875a2";
+// }
